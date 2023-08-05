a03b3ad1b92ca40bfebad274261e34ccc96e26c7
safwenourabi1 <140905862+safwenourabi1@users.noreply.github.com>
2023-08-05 17:38:54 +0100

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path,re_path
 from django.shortcuts import redirect
 from . import views
-from .views import handle_nonexistent_url
+
 
 urlpatterns = [
     path("main", views.main, name="main"),
@@ -12,6 +12,6 @@ urlpatterns = [
     path("main/imageencryption", views.show3, name="main/imageencryption"),
     path("main/electronicproject", views.show4, name="main/electronicproject"),
     path("", views.redir, name=""),
-    re_path(r'^.*$', handle_nonexistent_url, name='catch-all'),
+    re_path(r'^.*$', views.handle_nonexistent_url, name='catch-all'),
 
 ]
