28b2658a73faaeb97a058854c4534bb942400334
safwenourabi1 <140905862+safwenourabi1@users.noreply.github.com>
2023-08-05 14:51:27 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ pip install -r requirements.txt
 set -o errexit
 
 poetry install
-poetry add 'whitenoise[brotli]'
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+poetry add 'whitenoise[brotli]'
