2a01ab1d40487858f9587ca32ce307d4bd1d3ec3
safwenourabi1 <140905862+safwenourabi1@users.noreply.github.com>
2023-08-05 15:04:44 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 # exit on error
+curl -sSL https://install.python-poetry.org | python3 -
+[tool.poetry.dependencies]
+
+
+python = "^3.9.10"
+poetry add django
+
 pip install -r requirements.txt
 set -o errexit
 
 poetry install
+poetry add gunicorn
+poetry add 'whitenoise[brotli]'
 
 python manage.py collectstatic --no-input
 python manage.py migrate
