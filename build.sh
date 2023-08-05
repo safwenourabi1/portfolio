f4aa10ebc966416516674030911dd937672e945b
safwenourabi1 <140905862+safwenourabi1@users.noreply.github.com>
2023-08-05 15:17:23 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
-curl -sSL https://install.python-poetry.org | python3 -
-[tool.poetry.dependencies]
 
 
-python = "^3.9.10"
-poetry add django
+
+
 
 pip install -r requirements.txt
 set -o errexit
